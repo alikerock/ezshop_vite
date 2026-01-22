767c10bdfa17514a4d9e0024fc97c256a60ed040
alikerock <alikerock@gmail.com>
2026-01-22 11:30:20 +0900

    feat: 장바구니 정보 로컬스토리지에 저장

--- src/cart/CartContext.jsx
+++ src/cart/CartContext.jsx
@@ -1,11 +1,16 @@
-import { createContext, useContext, useState, useMemo } from "react";
+import { createContext, useContext, useState, useMemo, useEffect } from "react";
+
+const STORAGE_KEY = 'cartItems';
 
 //1. store 생성
 const CartContext = createContext(null);
 
-//2. Provider 생성
+//2. Provider 생성, 로컬스토리지에서 기존 장바구니 정보 조회 초기값 할당
 export function CartProvider({ children }) {
-  const [cartItems, setCartItems] = useState([]); //미션 1. 로컬스토리지에서 값을 초기값
+  const [cartItems, setCartItems] = useState(()=>{
+    const saved = window.localStorage.getItem(STORAGE_KEY);
+    return saved? JSON.parse(saved) : [];
+  }); 
 
   //장바구니에 상품 추가 함수
   const addItem = (item) => {
@@ -23,6 +28,10 @@ export function CartProvider({ children }) {
     ()=>cartItems.reduce((sum,n)=> sum + n.qty,0), [cartItems]
   ); //장바구니 상품 목록 변경되면, 수량 계산
 
+  //장바구니 항목 변경시 로컬스토리지에 저장
+  useEffect(()=>{
+    window.localStorage.setItem(STORAGE_KEY, JSON.stringify(cartItems));
+  },[cartItems]);
  
   // const value = useMemo(()=>{return {cartItems, addItem, count}}, [cartItems,count]);
   const value = useMemo(()=>({cartItems, addItem, count}), [cartItems,count]);
